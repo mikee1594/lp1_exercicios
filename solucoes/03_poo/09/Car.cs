64d972501e02e00ab75e287be696f43d42cedef6
Nuno Figueiredo <nunosilvares@hotmail.com>
2018-05-19 19:11:37 +0100

    Adicionar a solução do exercicio 09

---
+++ solucoes/03_poo/09/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09
+{
+    public enum FuelType { Gasoline, Diesel, LPG, Electric };
+
+    public class Car
+    {
+
+        public float Speed { get; private set; }
+        public float Weight { get; private set; }
+        public FuelType Fuel { get; private set; }
+        public static float MaxSpeed = 220.0F;
+
+        public Car(float weight, FuelType fuel)
+        {
+            Weight = weight;
+            Fuel = fuel;
+            Speed = 0;
+        }
+
+        public float Accelerate(float x)
+        {
+            Console.WriteLine($"\tAccelerating {x}");
+
+            Speed += x;
+
+            if (Speed > MaxSpeed)
+            {
+                Console.WriteLine($"\t\tHitted Max Speed ({MaxSpeed}), accelerated {x - (Speed - MaxSpeed)}");
+                Speed = MaxSpeed;
+            }
+            return Speed;
+        }
+
+        public float Break(float x)
+        {
+            Console.WriteLine($"\tBreaking {x}");
+
+            Speed -= x;
+
+            if (Speed < 0)
+            {
+                Console.WriteLine($"\t\tHitted Full Stop (0), breaked {Speed + x}");
+                Speed = 0;
+            }
+            return Speed;
+        }
+
+        public float GetSpeed()
+        {
+            return Speed;
+        }
+
+        public FuelType GetFuelType()
+        {
+            return Fuel;
+        }
+
+        public float GetWeight()
+        {
+            return Weight;
+        }
+
+        public static float GetMaxSpeed()
+        {
+            return MaxSpeed;
+        }
+
+        public static float SetMaxSpeed(float maxSpeed)
+        {
+            MaxSpeed = maxSpeed;
+
+            return maxSpeed;
+        }
+    }
+}
---
+++ solucoes/03_poo/09/TestCar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09
+{
+    class TestCar
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(1000, FuelType.Gasoline);
+
+            // Test GetFuelType, GetWeight , GetMaxSpeed
+            Console.WriteLine($"Car Fuel: {car.GetFuelType()}");
+            Console.WriteLine($"Car Weight: {car.GetWeight()}");
+            Console.WriteLine($"Car Max Speed: {Car.GetMaxSpeed()}");
+            Console.WriteLine();
+            // Test Accelerate, Break and GetSpeed
+            Console.WriteLine($"Speed: {car.GetSpeed()}");
+            car.Accelerate(50);
+            Console.WriteLine($"Speed: {car.GetSpeed()}");
+            car.Accelerate(500);
+            Console.WriteLine($"Speed: {car.GetSpeed()}");
+            car.Break(50);
+            Console.WriteLine($"Speed: {car.GetSpeed()}");
+            car.Break(500);
+            Console.WriteLine($"Speed: {car.GetSpeed()}");
+            Console.WriteLine();
+            // Test SetMaxSpeed
+            Console.WriteLine($"Car Max Speed: {Car.SetMaxSpeed(300)}");
+            car.Accelerate(500);
+        }
+    }
+}
